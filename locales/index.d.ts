61d8ceff06042ae9a9d97692ea29ba79d6def904
doraco <foyfoymaru@gmail.com>
2023-11-11 12:36:18 +0900

    どらごすきー:リアクション原寸大表示を追加(backspacekeyより取り込み)

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -719,6 +719,7 @@ export interface Locale {
     "loadRawImages": string;
     "disableShowingAnimatedImages": string;
     "highlightSensitiveMedia": string;
+    "allowReactionsBeams": string;
     "verificationEmailSent": string;
     "notSet": string;
     "emailVerified": string;
