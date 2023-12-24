c5851f36f86095b89a3d32f4f64f5f63fb1ecb0a
doraco <foyfoymaru@gmail.com>
2023-12-24 11:27:18 +0900

    Revert "どらごすきー:リアクション原寸大表示を追加(backspacekeyより取り込み)"
    This reverts commit 61d8ceff06042ae9a9d97692ea29ba79d6def904.

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -719,7 +719,6 @@ export interface Locale {
     "loadRawImages": string;
     "disableShowingAnimatedImages": string;
     "highlightSensitiveMedia": string;
-    "allowReactionsBeams": string;
     "verificationEmailSent": string;
     "notSet": string;
     "emailVerified": string;
